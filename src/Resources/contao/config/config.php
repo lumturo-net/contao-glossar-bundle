d2c25aa6b7d67fbaef8e230541e0e273a1f2f427
Christian Wederkda <cw@lumturo.net>
2021-06-16 15:41:40 +0200

    Assetpfade angepasst

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,6 +5,6 @@ use LumturoNet\ContaoGlossarBundle\Models\Entry;
 $GLOBALS['TL_MODELS']['tl_lt_glossar'] = Entry::class;
 $GLOBALS['BE_MOD']['lumturo']['glossar'] = ['tables' => ['tl_lt_glossar']];
 if(TL_MODE === 'FE') {
-    $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/glossar/glossar.js';
-    $GLOBALS['TL_CSS'][] = 'bundles/glossar/glossar.css';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaoglossar/glossar.js';
+    $GLOBALS['TL_CSS'][] = 'bundles/contaoglossar/glossar.css';
 }
