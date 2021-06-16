2189a6981da272246735f08b1e4eb258ab90eeed
Christian Wederkda <cw@lumturo.net>
2021-06-16 15:24:07 +0200

    + implements BundlePluginInterface

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,13 +3,14 @@ namespace LumturoNet\ContaoGlossarBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use LumturoNet\ContaoGlossarBundle\ContaoGlossarBundle;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-class Plugin implements RoutingPluginInterface
+class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
