97578ea14eeff68a9d0b5bc5d9fa19b5817c7f5b
Christian Wederkda <cw@lumturo.net>
2021-07-12 11:15:12 +0200

    Revert "v3.0 initial commit"
    This reverts commit 328d68a5

--- src/Controller/WordController.php
+++ src/Controller/WordController.php
@@ -6,7 +6,6 @@ use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\DataContainer;
 use Contao\StringUtil;
 use LumturoNet\ContaoGlossarBundle\Models\Entry;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class WordController
@@ -25,21 +24,6 @@ class WordController
         $objEntity = Entry::findBy('slug', StringUtil::generateAlias($strWord));
 
         return new Response($objEntity->description);
-    }
-
-    public function findAllAction()
-    {
-        $this->framework->initialize();
-
-        $jsonReturn    = [];
-        $objCollection = Entry::findAll();
-
-        /** @var Entry $item */
-        foreach($objCollection as $item) {
-            $jsonReturn[$item->title] = $item->description;
-        }
-
-        return new JsonResponse($jsonReturn);
     }
 
     public function generateSlugAction($mixedValue, DataContainer $objDataContainer)
