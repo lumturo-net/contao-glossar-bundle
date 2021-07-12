99da8c5d238878e6b292b5aa91c22e689dca57cd
Christian Wederkda <cw@lumturo.net>
2021-07-12 11:27:19 +0200

    restored v2.x

---
+++ src/Install.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LumturoNet\ContaoGlossarBundle;
+
+class Install
+{
+    public static function moveTinyMCETemplate()
+    {
+        copy(__DIR__ . '/Resources/templates/be_tinyMCE.html5', __DIR__ . '/../../../../templates/be_tinyMCE.html5');
+    }
+}
