328d68a5a48d8209ea25e37b490567cff34d1b2f
Christian Wederkda <cw@lumturo.net>
2021-07-12 11:13:19 +0200

    v3.0 initial commit

--- src/Controller/WordController.php
+++ src/Controller/WordController.php
@@ -6,6 +6,7 @@ use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\DataContainer;
 use Contao\StringUtil;
 use LumturoNet\ContaoGlossarBundle\Models\Entry;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class WordController
@@ -24,6 +25,21 @@ class WordController
         $objEntity = Entry::findBy('slug', StringUtil::generateAlias($strWord));
 
         return new Response($objEntity->description);
+    }
+
+    public function findAllAction()
+    {
+        $this->framework->initialize();
+
+        $jsonReturn    = [];
+        $objCollection = Entry::findAll();
+
+        /** @var Entry $item */
+        foreach($objCollection as $item) {
+            $jsonReturn[$item->title] = $item->description;
+        }
+
+        return new JsonResponse($jsonReturn);
     }
 
     public function generateSlugAction($mixedValue, DataContainer $objDataContainer)
